15ce77848d6378e397f333c77e4c4d0637803287
senathipathi228 <54020044+senathipathi228@users.noreply.github.com>
2019-08-12 14:06:50 +0530

    Update signup.php

--- mini-project/signup.php
+++ mini-project/signup.php
@@ -1,8 +1,8 @@
 <?php
 define('HOST','localhost');
- define('USERNAME', 'id9177877_nithish');
- define('PASSWORD','vns_1998');
- define('DB','id9177877_socializer');
+ define('USERNAME', 'id10472497_sena');
+ define('PASSWORD','senathipathi');
+ define('DB','id10472497_socializer');
 
 // Create connection
 $con = mysqli_connect(HOST,USERNAME,PASSWORD,DB);
